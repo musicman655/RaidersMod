31ee430ac6f9eac22c30e7c6e711345503f7a4f9
Andrzinho15 <59664190+Andrzinho15@users.noreply.github.com>
2020-05-22 16:37:18 -0300

    Update StingingSword.cs

--- Items/weapons/StingingSword.cs
+++ Items/weapons/StingingSword.cs
@@ -3,7 +3,7 @@ using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
-namespace RaidersMod.RaidersMod.Items.weapons
+namespace RaidersMod.Items.weapons
 {
 	public class StingingSword : ModItem
 	{
