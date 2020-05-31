38cf5a55c00efc03f1b9a85a8ecc4c3cb5b58469
AlmightyOdin <65109915+AlmightyOdin@users.noreply.github.com>
2020-05-31 16:50:22 +0530

    Added Slime Rifle

---
+++ Items/weapons/SlimeRifle.cs
@@ -0,0 +1,41 @@
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+namespace RaidersMod.Items.weapons
+{
+    public class SlimeRifle : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("SlimeRifle");
+            Tooltip.SetDefault(""); //Yogurt insert the tooltip here cuz idk what to put
+        }
+        public override void SetDefaults()
+        {
+            item.ranged = true;
+            item.autoReuse = true;
+            item.noMelee = true;
+            item.damage = 56;
+            item.knockBack = 1f;
+            item.shoot = ModContent.ProjectileType<Items.projectiles.SlimeFeather_Projectile>();
+            item.maxStack = 1;
+            item.shootSpeed = 15;
+            item.value = Item.buyPrice(0,1,20,0);
+            item.useTime = 4;
+            item.useStyle = ItemUseStyleID.HoldingOut;
+            item.useAnimation = 20;
+            item.reuseDelay = 24;
+            item.UseSound = SoundID.Item1;
+            item.width = 25;
+            item.height = 14;
+            item.scale *= 1.5f;
+        }
+        public override void OnHitNPC(Player player, NPC target, int damage, float knockBack, bool crit)
+        {
+            if(!target.friendly)
+            {
+                target.AddBuff(BuffID.Slow,600,false);
+            }
+        }
+    }
+}
