5a6e72c5d7bcf7a956316f439fccf7c7c196c4a5
AlmightyOdin <65109915+AlmightyOdin@users.noreply.github.com>
2020-07-10 18:24:03 +0530

    Commited minion weapon + fixed oozebrands location

---
+++ Items/weapons/Gelatinous_Remote.cs
@@ -0,0 +1,39 @@
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using RaidersMod.Items.projectiles.Minions;
+using Microsoft.Xna.Framework;
+namespace RaidersMod.Items.weapons
+{
+    public class Gelatinous_Remote : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Gelatinous Remote");
+            Tooltip.SetDefault("ae");//ae
+        }
+        public override void SetDefaults()
+        {
+            item.width = item.height = 20;
+            item.summon = true;
+            item.damage = 35;
+            item.knockBack = 1;
+            item.noMelee = true;
+            item.useStyle = ItemUseStyleID.HoldingOut;
+            item.rare = 4;
+            item.mana = 10;
+            item.maxStack = 1;
+            item.useTime = 25;
+            item.useAnimation = 25;
+
+            item.buffType = ModContent.BuffType<Buffs.Gelatinous_Buff>();
+            item.shoot = ModContent.ProjectileType<Gelatinous_Drone>();
+        }
+        public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+        {
+            player.AddBuff(item.buffType,2);
+            position = Main.MouseWorld;
+            return true;
+        }
+    }
+}
